1443916df0140089b5a326708f3e754a5ec69511
minhtuan1108 <minhtuan.1108tn@gamil.com>
2023-10-16 01:17:10 +0700

    done create feedback
    fixed feedback policy for creating feedback of worker

--- app/Http/Controllers/Api/v1/ReportController.php
+++ app/Http/Controllers/Api/v1/ReportController.php
@@ -35,15 +35,18 @@ class ReportController extends Controller
         return new NotAllowed(null);
     }
 
-    public function indexManager(){
+    public function indexManager()
+    {
         return ReportResource::collection(Report::orderBy('created_at', 'DESC')->paginate(30));
     }
 
-    public function indexUser(User $user){
+    public function indexUser(User $user)
+    {
         return ReportResource::collection($user->reports()->orderBy('created_at', 'DESC')->paginate(30));
     }
 
-    public function indexWorker(User $worker){
+    public function indexWorker(User $worker)
+    {
         return ReportResource::collection($worker->jobs()->orderBy('created_at', 'DESC')->paginate(30));
     }
 
@@ -61,8 +64,8 @@ class ReportController extends Controller
         $paths = [];
         $i = 1;
         $dir = $this->makeDir();
-        foreach ($files as $file){
-            $fileName = $file->storeAs($dir, "img$i".'_'.$report->id.'_'.sha1(time()).'.'.$file->extension());
+        foreach ($files as $file) {
+            $fileName = $file->storeAs($dir, "img$i" . '_' . $report->id . '_' . sha1(time()) . '.' . $file->extension());
             $paths[] = [
                 'media_link' => asset($fileName),
                 'local_file' => $fileName
@@ -73,15 +76,16 @@ class ReportController extends Controller
         return new ReportDetail(Report::with('medias')->find($report->id));
     }
 
-    private function makeDir(){
+    public function makeDir()
+    {
         $now = Carbon::now();
-        $dirNameYear = "photos/".$now->year;
-        $dirNameMonth = $dirNameYear ."/". $now->month;
+        $dirNameYear = "photos/" . $now->year;
+        $dirNameMonth = $dirNameYear . "/" . $now->month;
         // $dirExist = Storage::exists($dirNameYear) ? (Storage::exists($dirNameMonth) ? true : Storage::makeDirectory($dirNameMonth)) : (Storage::makeDirectory($dirNameMonth));
-        if(Storage::exists($dirNameYear)){
-            if(!Storage::exists($dirNameMonth))
+        if (Storage::exists($dirNameYear)) {
+            if (!Storage::exists($dirNameMonth))
                 Storage::makeDirectory($dirNameMonth);
-        }else{
+        } else {
             Storage::makeDirectory($dirNameMonth);
         }
         // echo $dirNameMonth;
@@ -102,27 +106,31 @@ class ReportController extends Controller
      */
     public function update(Request $request)
     {
-        $report = Report::find($request->idReport);
+        $report = Report::find($request->reports_id);
         //Check target to update
-        if($request->input('target') == 'ignore report'){
+        if ($request->input('target') == 'ignore report') {
             return $this->ignoreReport($request, $report);
-        }else{
-            if($request->input('target') == 'create assignment'){
-                return $this->updateToProcess($report);
-            }
-            
+        } elseif ($request->input('target') == 'create assignment') {
+            $report->status = ReportStatus::PROCESS;
+            $report->save();
+            return 'Cập nhật report thành process';
+        } elseif ($request->input('target') == 'create feedback') {
+            $report->status = ReportStatus::COMPLETE;
+            $report->save();
+            return 'Cập nhật report thành complete';
         }
-        
-        
+
+
     }
 
     //Ignore report
-    private function ignoreReport(Request $request, Report $report){
+    private function ignoreReport(Request $request, Report $report)
+    {
         //Check report status
-        if($report->status == ReportStatus::SENT){
+        if ($report->status == ReportStatus::SENT) {
             $this->authorize('update', $report);
             $report->feedback()->create([
-                'note' => $request->input('note', 'Không có lý do'),
+                'note' => $request->input('note', $request->input('note')),
                 'users_id' => $request->user()->id
             ]);
             $report->status = ReportStatus::IGNORE;
@@ -131,14 +139,16 @@ class ReportController extends Controller
                 'status' => 'sussess',
                 'message' => 'Từ chối báo cáo thành công!'
             ];
-        }else return [
-            'status' => 'fail',
-            'message' => 'Không thể từ chối báo cáo với trạng thái '. $report->status
-        ];
-    } 
+        } else
+            return [
+                'status' => 'fail',
+                'message' => 'Không thể từ chối báo cáo với trạng thái ' . $report->status
+            ];
+    }
 
     //Update report status to process
-    private function updateToProcess(Report $report){
+    private function updateToProcess(Report $report)
+    {
         $report->status = ReportStatus::PROCESS;
         $report->save();
         return 'Cập nhật report thành process';
@@ -147,33 +157,34 @@ class ReportController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Request $request ,string $id)
+    public function destroy(Request $request, string $id)
     {
         $report = Report::find($id);
         $this->authorize('delete', $report);
-        echo($report);
+        echo ($report);
         $this->hardDestroy($report);
     }
 
-    public function hardDestroy(Report $report){
-               
+    public function hardDestroy(Report $report)
+    {
+
         //Delete feedback and medias with its local file if exist
-        $feedbacks = $report->feedback()->get();  
-        foreach($feedbacks as $feedback){
+        $feedbacks = $report->feedback()->get();
+        foreach ($feedbacks as $feedback) {
             $feedback->hardDelete();
         }
 
         //Delete report's medias with its local file if exist
         $report_medias = $report->medias();
-        foreach($report_medias->get() as $media){
+        foreach ($report_medias->get() as $media) {
             $media->deleteLocalFile();
         }
         $report_medias->detach();
-        
+
         //Delete assignment if exist
         $report->assignment()->delete();
 
         //Delete report
         $report->delete();
-    } 
+    }
 }
