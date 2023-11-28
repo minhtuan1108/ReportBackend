63cc4ab2c72a2e8a54ad4a0c7e8c79018bf8e235
huykhaduy <huykhaduy@gmail.com>
2023-11-28 17:33:48 +0700

    fix lần này chắc chắn không bị bug

--- app/Http/Resources/Report/ReportDetail.php
+++ app/Http/Resources/Report/ReportDetail.php
@@ -33,21 +33,29 @@ class ReportDetail extends JsonResource
             ],
             'images' => MediaDetail::collection($this->whenLoaded('medias')),
             'created_at' => $this->created_at,
-            $this->mergeWhen($this->status == ReportStatus::SENT,[
-                'done_by' => [
-                    'message' => 'Đang gửi',
-                    'text' => 'Phản hồi của bạn đang được xử lý'
-                ]
-            ]),
-            $this->mergeWhen($this->status == ReportStatus::PROCESS, [
-                'done_by' => new AssignmentWorker($this->assignment)
-            ]),
-            $this->mergeWhen($this->status == ReportStatus::COMPLETE && isset($this->feedback[0]), [
-                'done_by' => new FeedbackDetail($this->feedback[0])
-            ]),
-            $this->mergeWhen($this->status == ReportStatus::IGNORE && isset($this->feedback[0]), [
-                'done_by' => new FeedbackIgnore($this->feedback[0])
-            ]),
+            $this->mergeWhen($this->status == ReportStatus::SENT, function() {
+                return [
+                    'done_by' => [
+                        'message' => 'Đang gửi',
+                        'text' => 'Phản hồi của bạn đang được xử lý'
+                    ]
+                ];
+            }),
+            $this->mergeWhen($this->status == ReportStatus::PROCESS, function(){
+                return [
+                    'done_by' => new AssignmentWorker($this->assignment)
+                ];
+            }),
+            $this->mergeWhen($this->status == ReportStatus::COMPLETE && isset($this->feedback[0]), function (){
+                return [
+                    'done_by' => new FeedbackDetail($this->feedback[0])
+                ];
+            }),
+            $this->mergeWhen($this->status == ReportStatus::IGNORE && isset($this->feedback[0]), function (){
+                return [
+                    'done_by' => new FeedbackIgnore($this->feedback[0])
+                ];
+            })
         ];
     }
 }
